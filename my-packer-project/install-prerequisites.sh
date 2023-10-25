1c25a4ce395e43b6df565a08cc93c0b7edcb53b5
andreissd <andrei.stoe@yahoo.com>
2023-10-25 22:00:24 +0300

    update files

--- my-packer-project/install-prerequisites.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-powershell -Command "Install-WindowsFeature -Name Web-Server"
---
+++ packer-project/prerequisites.ps1
@@ -0,0 +1,7 @@
+# Instalează aici toate prerechizitele necesare pentru aplicația ta
+# Exemplu: Instalează .NET Core, IIS, etc.
+
+# Instalare .NET Core
+Invoke-WebRequest -Uri https://dot.net/v1/dotnet-install.ps1 -OutFile $env:TEMP/dotnet-install.ps1 -UseBasicParsing
+. $env:TEMP/dotnet-install.ps1 -Channel Current -InstallDir $env:ProgramFiles\dotnet
+
